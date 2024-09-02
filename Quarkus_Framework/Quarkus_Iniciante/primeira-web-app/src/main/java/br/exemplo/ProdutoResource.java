a4c5d0b4cb82ee43945b7204ef7706cb165c1ef9
Samuel <samugit.projects@gmail.com>
2024-09-02 10:42:08 -0300

    Aula03 Endpoint GET com Parâmetro

--- Quarkus_Framework/Quarkus_Iniciante/primeira-web-app/src/main/java/br/exemplo/ProdutoResource.java
+++ Quarkus_Framework/Quarkus_Iniciante/primeira-web-app/src/main/java/br/exemplo/ProdutoResource.java
@@ -2,6 +2,7 @@ package br.exemplo;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @SuppressWarnings("all")
 @Path("/produto") // caminho da classe para acesso pelo navegador
@@ -11,5 +12,12 @@ public class ProdutoResource {
     @Path("/nome") // caminho específico, se remover essa linha com a anotação pode acessar pela rota da classe
     public String getNomeProduto() {
         return "Laptop";
+    }
+
+    @GET
+    @Path("/nome/{id}")
+    public String getNomeProdutoById(@PathParam("id") Integer id) {
+        if (id == 1) return "Laptop";
+        return "para esse id não existe produto";
     }
 }
