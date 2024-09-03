f2434b1d25aab40bcff0f20977577ce1ec8c4a90
Samuel <samugit.projects@gmail.com>
2024-09-03 10:37:38 -0300

    Aula04 Endpoint POST

--- Quarkus_Framework/Quarkus_Iniciante/primeira-web-app/src/main/java/br/exemplo/ProdutoResource.java
+++ Quarkus_Framework/Quarkus_Iniciante/primeira-web-app/src/main/java/br/exemplo/ProdutoResource.java
@@ -1,8 +1,12 @@
 package br.exemplo;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SuppressWarnings("all")
 @Path("/produto") // caminho da classe para acesso pelo navegador
@@ -19,5 +23,12 @@ public class ProdutoResource {
     public String getNomeProdutoById(@PathParam("id") Integer id) {
         if (id == 1) return "Laptop";
         return "para esse id não existe produto";
+    }
+
+    @POST
+    @Path("/nome")
+    public String postNomeProduto(String nomeProduto) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        return nomeProduto + " foi processado em: " + dateTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH'h'mm'min'ss'seg'")); // formato incluído por conta própria, na aula usou apenas o dateTime
     }
 }
