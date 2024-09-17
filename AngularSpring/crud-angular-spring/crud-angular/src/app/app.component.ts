e84b8875fda6279b96fb01e8b4183994f30e0ad4
Samuel <samugit.projects@gmail.com>
2024-09-16 23:23:55 -0300

    Angular/Spring Aula03 - Criando uma Toolbar na Página Principal

--- AngularSpring/crud-angular-spring/crud-angular/src/app/app.component.ts
+++ AngularSpring/crud-angular-spring/crud-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
